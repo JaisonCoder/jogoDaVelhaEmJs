d8b3c22e9627504e8d27680d66c9b4190ec33775
Jaison Farias <130522387+JaisonCoder@users.noreply.github.com>
2024-02-24 11:11:01 -0300

    agora um contador de vitorias foi adiconado, a cada fim de partida os jogadores vencedores recebem um ponto

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var jogador = "x";
-var coresJogadas = Array.from({length:9}, () => "");
+var coresJogadas = Array.from({ length: 9 }, () => "");
 var corJogadorX = "red";
 var corJogadorO = "blue";
+var vitoriasX = 0;
+var vitoriasO = 0;
+var elementoVitoriasX = document.getElementById("jogador-x")
+var elementoVitoriasO = document.getElementById("jogador-o")
+
 function alternarCor() {
     var quadrados = document.querySelectorAll(".quadrado");
     quadrados.forEach(function (celula, index) {
         celula.style.color = coresJogadas[index];
-        
+
     });
 
 }
@@ -19,10 +24,11 @@ function verificarGanhador() {
     var valores = [];
     quadrados.forEach(function (celula, index) {
         valores.push(celula.innerHTML);
-        if (celula.innerHTML !== ""){
+        if (celula.innerHTML !== "") {
             coresJogadas[index] = celula.innerHTML === "x" ? corJogadorX : corJogadorO;
         }
-        
+
+       
     });
 
     var combinacoesVencedoras = [
@@ -34,6 +40,15 @@ function verificarGanhador() {
         var [a, b, c] = combinacoesVencedoras[i];
         if (valores[a] !== "" && valores[a] === valores[b] && valores[a] === valores[c]) {
             alert("O Jogador " + valores[a] + " ganhou!");
+            if (valores[a] === "x"){
+                vitoriasX++;
+                elementoVitoriasX.textContent = vitoriasX;
+                localStorage.setItem("vitoriasX", vitoriasX);
+            }else{
+                vitoriasO++;
+                elementoVitoriasO.textContent = vitoriasO;
+                localStorage.setItem("vitoriasO", vitoriasO);
+            }
             return;
         }
     }
@@ -44,6 +59,14 @@ function verificarGanhador() {
             break;
         }
     }
+    if (localStorage.getItem("vitoriasX") !== null) {
+        vitoriasX = parseInt(localStorage.getItem("vitoriasX"));
+        elementoVitoriasX.textContent = vitoriasX;
+      }
+      if (localStorage.getItem("vitoriasO") !== null) {
+        vitoriasO = parseInt(localStorage.getItem("vitoriasO"));
+        elementoVitoriasO.textContent = vitoriasO;
+      }
     if (todosPreenchidos) {
         alert("Empatou!")
     }
@@ -52,7 +75,7 @@ function jogar(celula) {
     if (celula.innerHTML == "") {
         celula.innerHTML = jogador;
         verificarGanhador();
-        jogador = jogador === "x" ? "o" : "x"; 
+        jogador = jogador === "x" ? "o" : "x";
         alternarCor();
     }
 }
